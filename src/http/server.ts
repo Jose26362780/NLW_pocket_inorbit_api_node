22a226dec2b0714a5bb120ccc828cec7844ecaed
Jose Martinez <juniorjose1925@gmail.com>
2025-04-18 19:51:25 -0300

    initial commit and deploy of API

---
+++ src/http/server.ts
@@ -0,0 +1,35 @@
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './routes/create-goal'
+import { createCompletionRoute } from './routes/create-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+//aqui instalamos o cors 
+app.register(fastifyCors, {
+  origin:'*'// aqui vamos a colocar o link de nosso front end quando estiver em produção
+})
+
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+//Importando as routes
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
